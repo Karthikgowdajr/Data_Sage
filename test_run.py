1657506afc2aa13c3bc95b36b9c6c831c4fffdc5
Karthikjr <karthik7022551704@gmail.com>
2026-01-26 20:39:06 +0530

    Remove test_run file and sync with main

--- test_run.py
+++
@@ -1,8 +0,0 @@
-from app.file_loader import load_file
-from app.ai_engine import ask_data
-
-df = load_file("ipl_2023_dataset.csv")
-
-answer = ask_data(df, "give me a table of total spending by each team")
-
-print(answer)
